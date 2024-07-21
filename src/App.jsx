5f06fe87240ff1f4f9f5aff8995ba12a1f2381e8
Busra Mestan <bmestan50@gmail.com>
2024-07-21 23:46:33 +0300

    create react app

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+     <h1> Hello World</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam praesentium culpa mollitia ut deserunt quos id necessitatibus ratione nesciunt, iusto impedit fuga voluptatibus iste, nemo itaque earum corrupti laborum sit?</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
